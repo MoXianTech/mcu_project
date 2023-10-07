3db89a79d3c7ae47f50fa991f6087c3da45e2d38
yongtding <yongtding2-c@my.cityu.edu.hk>
2023-10-07 18:02:12 +0800

    NEW: 添加基础代码

---
+++ Software_32x64_Bed_selection_quick_mount_board/Project/Keil_project/output/circular_buffer.d
@@ -0,0 +1,5 @@
+.\output\circular_buffer.o: circular_buffer.c
+.\output\circular_buffer.o: circular_buffer.h
+.\output\circular_buffer.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\output\circular_buffer.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\output\circular_buffer.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
---
+++ 智数health_care_prg/11x44/Project/Keil_project/output/circular_buffer.d
@@ -0,0 +1,5 @@
+.\output\circular_buffer.o: ..\..\Bsp\circular_buffer.c
+.\output\circular_buffer.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\output\circular_buffer.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\output\circular_buffer.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
+.\output\circular_buffer.o: ..\..\Bsp\circular_buffer.h
