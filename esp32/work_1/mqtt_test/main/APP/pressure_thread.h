c01b1de9fdecc47e8b68dda8555cfa71308ce3c6
abbott.wei <abbott.wei@mxkj.net.cn>
2024-08-06 20:13:56 +0800

    delate esp32 project

--- esp32/work_1/mqtt_test/main/APP/pressure_thread.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PRESSURE_THREAD_H__
-#define __PRESSURE_THREAD_H__
-
-
-
-#endif
--- esp32/work_1/mqtt_test/main/APP/temp_humi_thread.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TEMP_HUMI_TREAD_H__
-#define __TEMP_HUMI_TREAD_H__
-
-#include "thread_init.h"
-
-void temp_humi_thread(void *pvparams);
-
-#endif
